b2789a2459324c76bf2beb3bb6d2d167ec6e695a
Farid Zouheir <zouheirjob@hotmail.com>
2025-09-01 18:56:34 +0200

    Create GapBuffer datastructure skeleton

---
+++ src/main/java/zhrfrd/jnotes/GapBuffer.java
@@ -0,0 +1,26 @@
+package zhrfrd.jnotes;
+
+public class GapBuffer {
+    private final int DEFAULT_GAP_SIZE = 1024;
+    private char[] buffer;
+
+    public GapBuffer() {
+        buffer = new char[DEFAULT_GAP_SIZE];
+    }
+
+    protected void insert(char c) {
+
+    }
+
+    protected void delete() {
+
+    }
+
+    protected void moveCursor() {
+
+    }
+
+    protected void resize() {
+
+    }
+}
