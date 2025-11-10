8aa4db005a9f7c377729fd26610645eb87cb1ae2
Farid Zouheir <zouheirjob@hotmail.com>
2025-11-10 22:53:27 +0100

    Revert bb771a7
    Undo and redo don't work properly

--- src/main/java/zhrfrd/jnotes/command/PasteCommand.java
+++ src/main/java/zhrfrd/jnotes/command/PasteCommand.java
@@ -38,13 +38,13 @@ public class PasteCommand implements Command {
 
     @Override
     public void undo() {
-        if (gapBuffer.getCharBeforeCursor() == '\0' || pastedText == null || pastedText.isEmpty()) {
-            return;
-        }
-
-        for (int i = 0; i < pastedText.length(); i ++) {
-            gapBuffer.deleteChar(Direction.LEFT);
-        }
+//        if (gapBuffer.getCharBeforeCursor() == '\0' || pastedText == null || pastedText.isEmpty()) {
+//            return;
+//        }
+//
+//        for (int i = 0; i < pastedText.length(); i ++) {
+//            gapBuffer.deleteChar(Direction.LEFT);
+//        }
     }
 }
 
